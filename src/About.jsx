1398dd8ca18e51c941e60b6069d6633e32cc13ff
saeed <saidmohamedsaid62@gamil.com>
2025-10-15 15:39:54 +0300

    11commit

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,7 @@
-export default function About(){
-    return(
+export default function About() {
+    return (
         <>
-        {/* <div className="theCardsAbout"> */}
+            {/* <div className="theCardsAbout"> */}
             {/* <div className="aboutBox2">
                 <div className="textAboutSkills">
 
@@ -13,24 +13,24 @@ export default function About(){
                 {/* </div> */}
                 {/* <div className="allInsideAboutBox"> */}
 
-                    {/* <div className="linesPer"> */}
-                    {/* <div className="photoAndTitle"> */}
-                    {/* </div> */}
-                    {/* </div> */}
-                    <div className="allInfo">
-                        <div className="frontEndTextBox">
+                {/* <div className="linesPer"> */}
+                {/* <div className="photoAndTitle"> */}
+                {/* </div> */}
+                {/* </div> */}
+                <div className="allInfo">
+                    <div className="frontEndTextBox">
 
                         <h1 className="frontEndText">Front-End<br /> Developer</h1>
-                        </div>
-                        <h4 style={{textAlign:"left"}} className="myAbout"><h4 style={{color:"#fde68a",display:"inline"}}>Hi👋</h4>, I'm Saeed a Front End React Developer from Egypt and a university student</h4>
-                        <h5 style={{textAlign:"left"}}>I'm currently learning and building projects using modern web technologies.<br />
-                     I have a solid foundation in HTML,CSS and Javascript, and I'm actively improving my skills in React, Typescript,and Bootstrap.<br />
-                     I'm always excited to learn more, improve my code, and work on real-world projects.</h5>
                     </div>
-                        {/* <img className="line line1"  src="/imgProfile/myImg4.jpg" alt="" /> */}
+                    <h4 style={{ textAlign: "left" }} className="myAbout"><h4 style={{ color: "#fde68a", display: "inline" }}>Hi👋</h4>, I'm Saeed a Front End React Developer from Egypt and a university student</h4>
+                    <h5 style={{ textAlign: "left" }}>I'm currently learning and building projects using modern web technologies.<br />
+                        I have a solid foundation in HTML,CSS and Javascript, and I'm actively improving my skills in React, Typescript,and Bootstrap.<br />
+                        I'm always excited to learn more, improve my code, and work on real-world projects.</h5>
                 </div>
+                <img className="line line1" src="/imgProfile/myImg4.jpg" alt="" />
+            </div>
             {/* </div> */}
-        {/* </div> */}
+            {/* </div> */}
         </>
     )
 }
