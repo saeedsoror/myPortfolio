c7ecb02916a4c6198d71964550bcce8205074aaa
saeed <saidmohamedsaid62@gmail.com>
2025-11-29 15:08:34 +0200

    commitInm

--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -1,4 +1,10 @@
+import AOS from "aos";
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 export default function TechStack(){
+    useEffect(()=>{
+        AOS.init({duration : 1000})
+    },[])
     return(
         <>
           <div id="skills" className="contTechTitle">
@@ -7,16 +13,17 @@ export default function TechStack(){
                 </div>
                 {/* <hr style={{width:"80vw"}}/> */}
                 <div className="scrolling">
-                    <div className="Stacks">
-                        <img src="/stackIcons/html-5.png" alt="" className="stackIcon" />
-                        <img src="/stackIcons/css-3.png" alt="" className="stackIcon" />
-                        <img src="/stackIcons/js.png" alt="" className="stackIcon" />
-                        <img src="/stackIcons/typescript.png" alt="" className="stackIcon" />
-                        <img src="/stackIcons/physics.png" alt="" className="stackIcon" />
+                    <div  className="Stacks">
+                        <img data-aos="fade" data-aos-delay="500" src="/stackIcons/html-5.png" alt="" className="stackIcon" />
+                        <img data-aos="fade" data-aos-delay="400" src="/stackIcons/css-3.png" alt="" className="stackIcon" />
+                        <img data-aos="fade" data-aos-delay="300" src="/stackIcons/js.png" alt="" className="stackIcon" />
+                        <img data-aos="fade" data-aos-delay="400" src="/stackIcons/typescript.png" alt="" className="stackIcon" />
+                        <img data-aos="fade" data-aos-delay="500" src="/stackIcons/physics.png" alt="" className="stackIcon" />
                         {/* <img src="/stackIcons/icons8-nextjs-96.png" alt="" className="stackIcon" /> */}
                         {/* <img src="/stackIcons/bootstrap.png" alt="" className="stackIcon" /> */}
                     </div>
                 </div>
+                {useEffect(()=>{AOS.refresh()},[])}
                 {/* <hr style={{width:"80vw"}}/> */}
             </div>
         </>
