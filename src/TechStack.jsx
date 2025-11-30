6adfff759e6c11d19e4c59cfba3f6d6c83e46838
saeed <saidmohamedsaid62@gamil.com>
2025-11-30 13:53:13 +0200

    iconEdit

--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -14,11 +14,11 @@ export default function TechStack(){
                 {/* <hr style={{width:"80vw"}}/> */}
                 <div className="scrolling">
                     <div  className="Stacks">
-                        <img data-aos="fade" data-aos-delay="500" src="/stackIcons/html-5.png" alt="" className="stackIcon" />
-                        <img data-aos="fade" data-aos-delay="400" src="/stackIcons/css-3.png" alt="" className="stackIcon" />
-                        <img data-aos="fade" data-aos-delay="300" src="/stackIcons/js.png" alt="" className="stackIcon" />
-                        <img data-aos="fade" data-aos-delay="400" src="/stackIcons/typescript.png" alt="" className="stackIcon" />
-                        <img data-aos="fade" data-aos-delay="500" src="/stackIcons/physics.png" alt="" className="stackIcon" />
+                        <img data-aos="fade" data-aos-delay="200" src="/stackIcons/html-5.png" alt="" className="stackIcon" />
+                        <img data-aos="fade" data-aos-delay="100" src="/stackIcons/css-3.png" alt="" className="stackIcon" />
+                        <img data-aos="fade" data-aos-delay="50" src="/stackIcons/js.png" alt="" className="stackIcon" />
+                        <img data-aos="fade" data-aos-delay="100" src="/stackIcons/typescript.png" alt="" className="stackIcon" />
+                        <img data-aos="fade" data-aos-delay="200" src="/stackIcons/physics.png" alt="" className="stackIcon" />
                         {/* <img src="/stackIcons/icons8-nextjs-96.png" alt="" className="stackIcon" /> */}
                         {/* <img src="/stackIcons/bootstrap.png" alt="" className="stackIcon" /> */}
                     </div>
